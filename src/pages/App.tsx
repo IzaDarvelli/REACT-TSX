39a5939d02384792bfbb5d997404f9bbddc6e3a0
IzaDarvelli <izailmasilva09@gmail.com>
2022-08-04 11:08:29 -0300

    modified: react-studies

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,41 +5,41 @@ import Stopwatch from '../components/Stopwatch/Stopwatch';
 import { ITasks } from '../types/Task';
 import style from './App.module.scss';
 
-function App() {                        //is  ITasks or array vazio
-    const [tasks, setTasks] = useState<ITasks[] | []>([]);
-    const [selector, setSelect] = useState<ITasks>();
+function App() {                         //is  ITasks or array vazio
+  const [tasks, setTasks] = useState<ITasks[] | []>([]);
+  const [selector, setSelect] = useState<ITasks>();
 
-    function selectTask(taskSelect: ITasks){
-      setSelect(taskSelect);
-      setTasks(tasksOld => tasksOld.map(task => ({
-        ...task,
-        selector: task.id === taskSelect.id ? true : false //operador ternário;
-      })));
-    }
+  function selectTask(taskSelect: ITasks) {
+    setSelect(taskSelect);
+    setTasks(tasksOld => tasksOld.map(task => ({
+      ...task,
+      selector: task.id === taskSelect.id ? true : false //operador ternário;
+    })));
+  }
 
-    function filledTask(){
-      setSelect(undefined);
-      if(selector){
-        setTasks(tasksOld => tasksOld.map(task => {
-          if(task.id === selector.id){
-            return {
-              ...task,
-              selector: false,
-              filled: true
-            }
+  function filledTask() {
+    setSelect(undefined);
+    if (selector) {
+      setTasks(tasksOld => tasksOld.map(task => {
+        if (task.id === selector.id) {
+          return {
+            ...task,
+            selector: false,
+            filled: true
           }
-          return task;
-        }))
-      }
+        }
+        return task;
+      }))
     }
+  }
 
   return (
     <div className={style.AppStyle}>
       <Forms setTasks={setTasks} />
-      <List tasks={tasks}  selectTask={selectTask}/>
-      <Stopwatch 
-      selector={selector} 
-      filledTask={filledTask}/>
+      <List tasks={tasks} selectTask={selectTask} />
+      <Stopwatch
+        selector={selector}
+        filledTask={filledTask} />
     </div>
   );
 }
