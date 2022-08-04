ffc31b91689ece3e1380ad55d28aa05f7def9e60
IzaDarvelli <izailmasilva09@gmail.com>
2022-08-04 11:06:04 -0300

    Refatoração do código

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,22 +1,20 @@
 import React from "react";
 import style from './Button.module.scss';
 
-class Button extends React.Component<{ 
+interface Props {
     type?: "button" | "submit" | "reset" | undefined //props opicional
-    onClick?: () => void
-    children: string}> {
-    render() {
-         const {type = "button", onClick} = this.props;
-        return (
-            <button onClick={onClick} type={type} className={style.button}>
-                {this.props.children}
-            </button>
-        )
-    }
+    onClick?: () => void,
+    children?: React.ReactNode
+}
+
+function Button({ onClick, type, children }: Props) {
+    return (
+        <button onClick={onClick} type={type} className={style.button}>
+            {children}
+        </button>
+    )
 }
 
 export default Button;
-
-
 
 //type children, esse REACT.COMPONENT já espera que tenha esse type children. No DOM parent e child. "permissão para conseguirmos utilizar alguma coisa dentro do componente, que é o que nós queremos. "
--- src/components/Forms/Forms.tsx
+++ src/components/Forms/Forms.tsx
@@ -1,71 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { ITasks } from "../../types/Task";
 import Button from "../Button/Button";
 import style from './Forms.module.scss';
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 
-
-class Forms extends React.Component<{
+interface Props {
     setTasks: React.Dispatch<React.SetStateAction<ITasks[]>>
-}> {
-    //he always is a object:
-    state = {
-        task: "",
-        time: "00:00:00"
+}
 
-    }
+function Forms({ setTasks }: Props) {
+    const [task, setTask] = useState("");
+    const [time, setTime] = useState("00:00:00");
 
-    addTask(event: React.FormEvent<HTMLFormElement>) {
+    function addTask(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
-        this.props.setTasks(taskOld => [...taskOld, { ...this.state, select: false, filled: false, id: uuidv4()}]);
-        //resetar o forms:
-        this.setState({
-            task: "",
-            time: "00:00:00"
-        })
-        //console.log('state', this.state)
+        setTasks(taskOld => [...taskOld, {
+            task,
+            time,
+            select: false,
+            filled: false,
+            id: uuidv4()
+        }]);
+
+        setTask("");
+        setTime("00:00");
     }
+    return (
+        <form className={style.newTask} onSubmit={addTask}>
 
-    render() {
-        return (
-            <form className={style.newTask} onSubmit={this.addTask.bind(this)}>
+            <div className={style.inputContainer}>
+                <label htmlFor="task">
+                    Add a new studies
+                </label>
+                <input
+                    type="text"
+                    name="task"
+                    id="task"
+                    value={task}
+                    onChange={event => setTask(event.target.value)}
+                    placeholder="What do you want study?"
+                    required />
+            </div>
 
-                <div className={style.inputContainer}>
-                    <label htmlFor="task">
-                        Add a new studies
-                    </label>
-                    <input
-                        type="text"
-                        name="task"
-                        id="task"
-                        value={this.state.task}
-                        onChange={event => this.setState({ ...this.state, task: event.target.value })}
-                        placeholder="What do you want study?"
-                        required />
-                </div>
-
-                <div className={style.inputContainer}>
-                    <label htmlFor="time">
-                        Time
-                    </label>
-                    <input
-                        type="time"
-                        step="1"
-                        name="time"
-                        value={this.state.time} //sem value aleatório
-                        onChange={event => this.setState({ ...this.state, time: event.target.value })} //assim que normalmente pegamos o value do input
-                        id="time"
-                        min="00:00:00"
-                        max="01.30:00"
-                        required />
-                </div>
-                <Button type="submit">
-                    Add
-                </Button>
-            </form>
-        )
-    }
+            <div className={style.inputContainer}>
+                <label htmlFor="time">
+                    Time
+                </label>
+                <input
+                    type="time"
+                    step="1"
+                    name="time"
+                    value={time} //sem value aleatório
+                    onChange={event => setTime(event.target.value)} //assim que normalmente pegamos o value do input
+                    id="time"
+                    min="00:00:00"
+                    max="01.30:00"
+                    required />
+            </div>
+            <Button type="submit">
+                Add
+            </Button>
+        </form>
+    )
 }
 
 export default Forms;
